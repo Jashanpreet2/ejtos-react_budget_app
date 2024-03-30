22cc394bf68ddb00a2e56ab99596db89fc1b8c97
Jashanpreet Singh <jashan20preet21@gmail.com>
2024-03-30 12:23:41 -0400

    Add budget and remaining components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -16,11 +16,11 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
-                            /* Add Budget component here */
+                            <Budget/>
                         }        
 
                         {
-                            /* Add Remaining component here*/
+                            <Remaining/>
                         }        
 
                         {
