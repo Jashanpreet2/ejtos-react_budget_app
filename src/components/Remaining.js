be4df549ecb7474aef2b1f3ff80f142307423d4a
Jashanpreet Singh <jashan20preet21@gmail.com>
2024-03-30 12:23:31 -0400

    Make remaining component

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,20 @@
+import { AppContext } from "../context/AppContext";
+import { useContext } from "react";
 
+export default function Remaining() {
+  let { budget, expenses, currency } = useContext(AppContext);
+  let totalExpenses = expenses.reduce((total, expense) => {
+    total = total + expense.cost;
+    return total;
+  }, 0);
+  let remaining = budget - totalExpenses;
+
+  return (
+    <div class="alert alert-primary">
+      <span>
+        Remaining: {currency}
+        {remaining}
+      </span>
+    </div>
+  );
+}
